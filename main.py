58c76d294857a045075064c06c609b6c3b038edb
AlecsandraMuha <alecsandramuha@yahoo.com>
2021-10-06 19:18:34 +0300

    commit

--- main.py
+++ main.py
@@ -53,17 +53,55 @@ def test_is_superprime():
         print(' nu este superprim')
     assert(is_superprime(233)) == True
     assert(is_superprime(237)) == False
+def is_antipalindrome(n):
+    '''
+    Determina daca un numar dat n este antipalindrom
+    :param n:numarul dat
+    :return: True daca n este antipalindrom si False in cazul in care n nu este antipalindrom
+    '''
+
+    nrc = 0
+    x = n
+    while x!= 0:
+        nrc= nrc + 1
+        x = x// 10
+    x = n
+    inv = 0
+    while x != 0:
+        inv = inv * 10 + x % 10
+        x = x // 10
+    i = nrc // 2
+    while i != 0:
+        if n % 10 == inv % 10:
+            return False
+        i = i - 1
+        n = n // 10
+        inv = inv // 10
+    return True
+
+
+def test_is_antipalindrome():
+    n = int(input('Introduceti un numar:'))
+    if is_antipalindrome(n):
+        print(' este antipalindrom')
+    else:
+        print(' nu este antipalindrom')
+    assert is_antipalindrome(1234) == True
+    assert is_antipalindrome(2773) == False
 def main():
     while True:
         print("Optiunea 1.Determină dacă un număr dat este palindrom")
         print("Optiunea 2.Determină dacă un număr este superprim")
-        print("Optiunea 3.iesire")
+        print("Optiunea 3.Determină dacă un număr dat este antipalindrom")
+        print("Optiunea 4.iesire")
         option = input("selectati optiunea: ")
         if option == "1":
             test_is_palindrome()
         elif option == "2":
             test_is_superprime()
         elif option == "3":
+            test_is_antipalindrome()
+        elif option == "4":
             break
         else:
             print("optiune gresita.selecteaza alta optiune")
